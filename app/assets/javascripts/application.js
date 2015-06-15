8489fdbad5dd0305e2ebbe6c91bd8b9b966e2f36
Max DePriest <maxd924@gmail.com>
2015-06-14 22:04:17 -0500

    Got a Google map to show, but was getting an error.  Sorting that out

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+<script src="map/gmap.js"
--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,6 @@ class UserSessionsController < ApplicationController
   def create
     if @user = login(params[:email], params[:password])
       $veg = @user.email
-      $rabbit = @user.vegetarian
       redirect_back_or_to('/user', notice: 'Login successful')
     else
       flash.now[:alert] = 'Login failed'
