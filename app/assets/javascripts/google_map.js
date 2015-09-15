0a5909393ab44f6cd1bae9b86c8dc6860ec00b35
Max DePriest <maxd924@gmail.com>
2015-09-14 19:33:25 -0500

    trying to get directions was a nightmare with Google API

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -1,69 +1,7 @@
-// All this is for the HTML5 Geolocation feature. Ran out of time before could 
-// combine it with the address locator to show directions from the user's position
-
-// var map;
-
-// Finds the person's location on the map using HTML5 geolocation
-// function initialize() {
-//   var mapOptions = {
-//     zoom: 15
-//   };
-//   map = new google.maps.Map(document.getElementById('map-canvas'),
-//       mapOptions);
-
-//   // Try HTML5 geolocation
-//   if(navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(function(position) {
-//       new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-//       var infowindow = new google.maps.InfoWindow({
-//         map: map,
-//         position: pos,
-//         content: 'You are here.'
-//       });
-
-//       map.setCenter(pos);
-//     }, function() {
-//       handleNoGeolocation(true);
-//     });
-
-//   } else {
-//     // Browser doesn't support Geolocation
-//     handleNoGeolocation(false);
-//   }
-// }
-
-// function handleNoGeolocation(errorFlag) {
-//   if (errorFlag) {
-//     var content = 'Error: The Geolocation service failed.';
-//   } else {
-//     var content = 'Error: Your browser doesn\'t support geolocation.';
-//   }
-
-//   var options = {
-//     map: map,
-//     position: new google.maps.LatLng(60, 105),
-//     content: content
-//   };
-
-//   var infowindow = new google.maps.InfoWindow(options);
-//   map.setCenter(options.position);
-// }
-
-// function loadScript() {
-//   var script = document.createElement('script');
-//   script.type = 'text/javascript';
-//   script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
-//       '&signed_in=true&callback=initialize';
-//   document.body.appendChild(script);
-// }
-
-//Trying to load the map at the restaurant's geolocation
 
 var map;
 
 function initialize() {
-
   findCoords();
 
 }
@@ -75,6 +13,8 @@ function findCoords () {
             dataType: 'json',
 
           success: function(data, textStatus, jqXHR) {
+            //var myLocation = "600 Congress Avenue, Austin, Texas";
+            //findMe(myLocation);
             var longitude = data['results'][0]['geometry']['location']['lng'];
             var latitude = data['results'][0]['geometry']['location']['lat'];
             var myLatLng = new google.maps.LatLng(latitude, longitude);
